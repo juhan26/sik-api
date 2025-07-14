7030f8bc93dda3762778eb7c606114e4576f932d
juhn <macbook@macbooks-MacBook-Pro.local>
2025-07-15 02:10:37 +0800

    update home website

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import sequelize from './models';
 import userRoutes from './routes/userRoutes';
 import configRoutes from './routes/configRoutes';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
+
+// Serve static files from public
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/users', userRoutes);
 app.use('/api/config', configRoutes);
